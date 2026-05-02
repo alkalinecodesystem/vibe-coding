eb1e9fd8e03ad1aab973c1dfa31f9c210719ff60
alkalinecodesystem <149242380+alkalinecodesystem@users.noreply.github.com>
2026-05-02 23:21:00 +0200

    Delete music-service/music-service directory

--- music-service/music-service/src/main/java/com/example/musicservice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.musicservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
